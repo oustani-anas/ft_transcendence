c9f14181164172317ace0116cba18ac90058c805
oustani-anas <anas.migos20@gmail.com>
2023-11-24 21:22:18 +0100

    start

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+@Controller('sample')
+export class SampleController {
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getSample(): string {
+    return 'This is a sample NestJS API!';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { SampleController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [SampleController],
+  providers: [],
 })
 export class AppModule {}
